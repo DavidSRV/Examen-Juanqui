40b1f6bff192cc5168ef084ab0654051318823a3
David Ruiz <david.ruizvargas@hotmail.com>
2022-07-30 03:04:17 -0500

    "feat: se termina código"

--- src/Controllers/pacientesControllers.js
+++ src/Controllers/pacientesControllers.js
@@ -1,55 +1,77 @@
 const pacientesControllers = {};
 const e = require("express");
 
-pacientesControllers.getAll = (req,res) => {
+pacientesControllers.getAll = (req, res) => {
 
-};
+}
 
-pacientesControllers.create = (req,res) => {
-    
-        let peso_actual = [];
-        const perdieron_peso = 0;
-        const ganaron_peso = 0;
-        const pesoi_pesointermedio = 0;
-        const pacientes_objetivo = 0;
-        const objetivos = [];
-    
-        const pacientes = req.body;
-    
+pacientesControllers.create = (req, res) => {
+
+    let peso_actual = [];
+    let perdieron_peso = 0;
+    let cumplieron_objetivo = 0;
+    let response = {};
+
+    const pacientes = req.body;
+
     for (let i = 0; i < pacientes["pacientes"].length; i++) {
-       
-        let pacientesdatos = pacientes["pacientes"][i];
+        const pacientesdatos = pacientes["pacientes"][i];
 
-        let pesofinal = pacientesdatos.peso_inicial - pacientesdatos.peso_final;
+        //Punto A) Pacientes que perdieron o ganaron peso
 
-        let pacientesobjeto = {};
+        //Punto C) Cantidad de pacientes que alcazaron su objetivo
+         
+        let pacientesganaron = {};
+        
+        if ( pacientesdatos.peso_final > pacientesdatos.peso_inicial ) {
 
-        if(pesofinal >= 0){
-
-            pacientesobjeto.nombre = pacientesdatos.nombre;
-            pacientesobjeto.peso = pesofinal
-            pacientesobjeto.estado = "gano"
-            ganaron_peso++;
+            let pesofinal = pacientesdatos.peso_final - pacientesdatos.peso_inicial;
             
-            peso_actual.push(pacientesobjeto);
+            pacientesganaron.nombre = pacientesdatos.nombre;
+            pacientesganaron.pesoActual = pacientesdatos.peso_final;
+            pacientesganaron.estado = `gano ${pesofinal} kilos`;
 
-        }else if(pesofinal <= 0){
+            let estado = "subir";
 
-            pacientesobjeto.nombre = pacientesdatos.nombre;
-            pacientesobjeto.peso= pesofinal
-            pacientesobjeto.estado = "perdio"
-            perdieron_peso++;
+            estado === pacientesdatos.objetivo ? pacientesganaron.objetivo = "Cumplido" : pacientesganaron.objetivo = "No cumplido";
 
-            peso_actual.push(pacientesobjeto);
-
+            pacientesganaron.objetivo === "Cumplido" ? cumplieron_objetivo++ : 0;
+            
+            peso_actual.push(pacientesganaron) 
+            
         }
 
+        let pacientesperdieron = {};
+
         
+        if (pacientesdatos.peso_final < pacientesdatos.peso_inicial) {
+
+            let pesofinal = pacientesdatos.peso_inicial - pacientesdatos.peso_final;
+            
+            pacientesperdieron.nombre = pacientesdatos.nombre;
+            pacientesperdieron.pesoActual = pacientesdatos.peso_final;
+            pacientesperdieron.estado = `perdio ${pesofinal} kilos`;
+
+            let estado = "bajar";
+
+            estado === pacientesdatos.objetivo ? pacientesperdieron.objetivo = "Cumplido" : pacientesperdieron.objetivo = "No cumplido";
+
+            pacientesperdieron.objetivo === "Cumplido" ? cumplieron_objetivo++ : 0;
+
+            peso_actual.push(pacientesperdieron);
+        }
+
+        //Punto B) Pacientes que perdieorn peso entre la pesada inicial y la intermedia
+
+        pacientesdatos.peso_intermedio < pacientesdatos.peso_inicial ? perdieron_peso++ : 0;
+
     }
+    response["Peso Actual y Objetivos"] = peso_actual;
+    response["Perdieron Peso Inicio - Intermedio"] = perdieron_peso;
+    response["Cantidad de Usuarios que Cumplieron Objetivo"] = cumplieron_objetivo;
 
-    res.json(peso_actual);
+    res.json(response);
 
-    
-};
+}
 
 module.exports = pacientesControllers;
